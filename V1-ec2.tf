2d9d469ec71f588a1642238c373ce567dda201bb
deependrathakur25 <deependra.pratap214@gmail.com>
2024-04-17 20:29:17 +0530

    first commit

---
+++ V1-ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+
+  ami           = "ami-051f8a213df8bc089"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+
+}
